928e1c138ec7b0f188305441a192ce745c365a04
Ayubxon <aubsattorov@gmail.com>
2025-03-02 22:12:17 +0500

    Add initial project structure with HTML, CSS, JavaScript, and assets

---
+++ app.js
@@ -0,0 +1,12 @@
+// const drums = document.querySelectorAll(".drum");
+
+// drums.forEach((drum) => {
+//   drum.addEventListener("click", () => {
+//     if (drum.textContent == "w") {
+//     }
+//   });
+// });
+// uy ishi
+// 52
+// 58
+// 59
